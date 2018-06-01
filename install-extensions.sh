071ebce36c6da9102639cba75f0fa85e1d5040f3
Erik Svensson <eriksven@Eriks-Mac-mini.local>
2018-06-02 00:30:26 +0200

    Bash script containing output of code --list-extensions.

---
+++ install-extensions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension Ionide.Ionide-fsharp
+code --install-extension eamodio.gitlens
+code --install-extension jakeboone02.cypher-query-language
+code --install-extension ms-vscode.csharp
